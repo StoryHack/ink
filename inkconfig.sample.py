2e42d3e5fd2bca74f8262409f10568582a51a3eb
Ben Crowder <ben.crowder@gmail.com>
2011-06-02 15:10:45 -0600

    Fixed bug in the sample inkconfig

--- inkconfig.sample.py
+++ inkconfig.sample.py
@@ -4,7 +4,7 @@ inkconfig = {
 	'site_title': 'Your Site Title Here',
 	'site_url': 'http://yoururlhere.com',
 	'site_desc': 'Description of your site.',			# for RSS
-	'site_target': 'user@host:/path/to/files'			# for rsync
+	'site_target': 'user@host:/path/to/files',			# for rsync
 	'syspath': '/where/your/files/are/stored/locally',	# without trailing slash
 	'imagewidth': 525,									# max width for images
 	'editor': 'vim',									# for use in "ink edit [slug]"
